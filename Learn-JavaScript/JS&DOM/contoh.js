756150092fba89bf8d839b0c19a7e2c168b673ba
Rafa <rafacrimson2@gmail.com>
2025-07-07 08:53:57 +0800

    study at 11.01.25

---
+++ Learn-JavaScript/JS&DOM/contoh.js
@@ -0,0 +1,23 @@
+// penggunaan getElementById            (Element)
+const judul = document.getElementById('judul');
+judul.style.color = 'red';
+judul.style.backgroundColor = '#ccc';
+judul.innerHTML = 'Rafa readst'     //mengubah text nya
+
+
+// penggunaan getElementsByTagName      (HTMLCollection)
+const p = document.getElementsByTagName('p');
+// p[2].style.backgroundColor = 'lightblue' ini yg cuma 1 1 aja
+for(let i = 0;i < p.length; i++){
+    p[i].style.backgroundColor = 'lightblue';
+}
+
+const h1 = document.getElementsByTagName('h1')[0];
+h1.style.fontSize = '80px';
+
+
+// penggunaan getElementsByClassName      (HTMLCollection)
+const p1 = document.getElementsByClassName('p1')[0];
+p1.innerHTML = 'sdh berubah kah';
+
+
