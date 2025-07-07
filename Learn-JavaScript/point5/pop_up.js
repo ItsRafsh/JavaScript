4e7f5246015d59ad964bc8565a332ceb763155bc
Rafa <rafacrimson2@gmail.com>
2025-07-07 08:48:33 +0800

    ...07.24

---
+++ Learn-JavaScript/point5/pop_up.js
@@ -0,0 +1,52 @@
+// alert('Hello World!');
+
+
+
+// alert('Halo');
+// alert('Nama Saya');
+// alert('Rafa');
+// alert('Haris');
+// alert(':) hehe');
+
+// var nama = prompt('Masukin namamu');
+// alert(nama);
+
+
+
+// var tes = confirm('kamu yakiinn??');
+// alert(tes);
+
+
+
+
+// var tes = confirm('Kamu Yakinnn??');
+
+// if( tes === true) {
+//     alert('user menekan YESSS');
+// } else {
+//     alert('User menekan BATALLL');
+// }
+
+
+
+
+alert('selamat datang');
+var lagi = true;
+
+while( lagi ) {
+    var nama = prompt('masukinn nama muuu:');
+    alert('halo ' + nama);
+
+    lagi = confirm('coba lagi');
+}
+
+alert('TERIMA KASIHH!!');
+
+
+
+
+
+
+
+
+
